f428f91410e93fdcc29d9d4be56f132e87092fb6
BacktoBasic1 <akashi1234456@gmail.com>
2023-09-09 03:22:55 +0700

    Update db.config.js

--- nodejs/app/config/db.config.js
+++ nodejs/app/config/db.config.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('reactjs_todo', 'phuongnguyen', 'qweqwe123', {
   dialect: 'mysql',
-  host: 'localhost',
+  host: 'db4free.net',
   port: 3306,
   logging: false, // tắt log
 });
